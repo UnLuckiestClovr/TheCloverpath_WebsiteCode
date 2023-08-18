a654c2286d72364f4687e4a889ae35b0e89dcc01
UnLuckiestClovr <129197699+UnLuckiestClovr@users.noreply.github.com>
2023-08-17 19:14:07 -0600

    Website Changes

--- edmserver/routes/FlowerShopPage.js
+++ edmserver/routes/FlowerShopPage.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function() {
+router.get('/', function(req, res) {
     res.render('FlowerShopPage', {title: "The Cloverpatch"})
 })
 
