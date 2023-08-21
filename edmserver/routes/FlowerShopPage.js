9282ebd5a7a43f911c1f7369f5557e7cc5b9e5cf
UnLuckiestClovr <129197699+UnLuckiestClovr@users.noreply.github.com>
2023-08-21 13:26:41 -0600

    Order Page Start

--- edmserver/routes/FlowerShopPage.js
+++ edmserver/routes/FlowerShopPage.js
@@ -5,23 +5,4 @@ router.get('/', function(req, res) {
     res.render('FlowerShopPage', {title: "The Cloverpatch"})
 })
 
-// var buttons = document.querySelectorAll('orderButton')
-// buttons.forEach(function(button) {
-//     button.addEventListener('click', OrderFlowers)
-// })
-
-// const OrderFlowers = async (FlowerSpecies) => {
-//     console.log("Clicked Order on: ", FlowerSpecies)
-
-//     orderOverlayOn()
-// }
-
-// const orderOverlayOn = () => {
-//     document.getElementById("").style.display = "block";
-// }
-
-// const orderOverlayOff = () => {
-//     document.getElementById("").style.display = "none";
-// }
-
 module.exports = router;
