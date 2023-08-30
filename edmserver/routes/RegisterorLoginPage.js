65a69ab2bf50df94d28493ee08e8cec528e22bbf
UnLuckiestClovr <129197699+UnLuckiestClovr@users.noreply.github.com>
2023-08-29 19:14:46 -0600

    We Ballin

--- edmserver/routes/RegisterorLoginPage.js
+++ edmserver/routes/RegisterorLoginPage.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-    res.render('RegisterorLoginPage', {title: "The Cloverpatch"})
+    res.render('RegisterorLoginPage', {title: "The Cloverpatch", loggedInBool: true})
 })
 
 module.exports = router;
