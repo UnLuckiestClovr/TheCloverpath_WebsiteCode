c2555cbf2556369fe9b4fe50015d08c4c60f1889
UnLuckiestClovr <urluckyclovr@gmail.com>
2023-08-31 12:36:31 -0600

    Sync to Laptop

--- edmserver/public/javascripts/edm.js
+++ edmserver/public/javascripts/edm.js
@@ -117,3 +117,51 @@ function changeViewtoRegister() {
     RegisterView.style.display = "block"
     LoginView.style.display = "none"
 }
+
+// - - - - - - Questionaire Script - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+const subQuestionAnswersBTN = document.getElementById('SubmitQuestionaire')
+const questionsInvalid = document.getElementById('questionInvalidOutput')
+
+subQuestionAnswersBTN.addEventListener('click', function () {
+    const q1 = document.getElementById('question1')
+    const q2 = document.getElementById('question2')
+    const q3 = document.getElementById('question3')
+
+    if(q1.value === "" || q2.value === "" || q3.value === "") {
+        invalidQuestionaire(q1.value, q2.value, q3.value)
+        return
+    }
+    questionsInvalid.innerHTML = ""
+
+    username = 
+
+    const questionAnswers = {
+
+    }
+
+    try{
+        const response = await fetch('/users/updatequestionaire', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(questionAnswers)
+        })
+    }
+})
+
+function invalidQuestionaire(ans1, ans2, ans3) {
+    let textVar = "<article>"
+    if(ans1 === "") {
+        textVar += "<p> Question 1 Requires Input </p>"
+    }
+    if(ans2 === "") {
+        textVar += "<p> Question 2 Requires Input </p>"
+    }
+    if(ans3 === "") {
+        textVar += "<p> Question 3 Requires Input </p>"
+    }
+    textVar += "</article>"
+    questionsInvalid.innerHTML = textVar
+}
